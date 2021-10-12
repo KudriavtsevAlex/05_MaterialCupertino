3aa4bdebad7c0767b5cecd82f1c970c10b8bf0a4
Александр Кудрявцев <aleksandrkudravcev@MacBook-Pro-Aleksandr.local>
2021-10-12 08:51:57 +0300

    all works except aligment

--- lib/scaffold_example.dart
+++ lib/scaffold_example.dart
@@ -1,27 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-class ScaffoldExample extends StatefulWidget {
-  const ScaffoldExample({Key? key}) : super(key: key);
+class ScaffoldEx extends StatefulWidget {
+  const ScaffoldEx({Key? key}) : super(key: key);
 
   @override
-  _ScaffoldExampleState createState() => _ScaffoldExampleState();
+  _ScaffoldExState createState() => _ScaffoldExState();
 }
 
-class _ScaffoldExampleState extends State<ScaffoldExample> {
+class _ScaffoldExState extends State<ScaffoldEx> {
   int tabIndex = 0;
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
-  void openDrawer() {
-    scaffoldKey.currentState!.openDrawer();
-  }
-
-  void openEndDrawer() {
-    scaffoldKey.currentState!.openEndDrawer();
-  }
-
   void openBottomSheet() {
-    scaffoldKey.currentState!.showBottomSheet((context) => FirstPage());
+    scaffoldKey.currentState!.showBottomSheet((context) => BottomSheet());
   }
 
   @override
@@ -29,91 +21,11 @@ class _ScaffoldExampleState extends State<ScaffoldExample> {
     return MaterialApp(
       home: Scaffold(
         key: scaffoldKey,
-        appBar: AppBar(
-          title: Text('App bar title!'),
-          actions: [
-            Builder(
-              builder: (context) => IconButton(
-                icon: Icon(Icons.person),
-                onPressed: () {},
-                // onPressed: toggleBottomSheet,
-              ),
-            )
-          ],
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: CircleAvatar(
-                  radius: 68,
-                  backgroundColor: Colors.cyan,
-                ),
-              ),
-              ListTile(
-                leading: const Icon(Icons.home),
-                title: const Text('Item 1'),
-                onTap: () {},
-                onLongPress: () {
-                  print('Long press');
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.backpack),
-                title: const Text('Item 2'),
-                onTap: () {},
-              ),
-            ],
-          ),
-        ),
-        endDrawer: const Drawer(
-          child: Center(
-            child: Text('EndDrawer'),
-          ),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              TextButton(
-                  onPressed: () {
-                    openDrawer();
-                  },
-                  child: Text('Open Drawer')),
-              TextButton(
-                  onPressed: () {
-                    openEndDrawer();
-                  },
-                  child: Text('Open EndDrawer')),
-              TextButton(
-                  onPressed: () {
-                    openBottomSheet();
-                  },
-                  child: Text('Open BottomSheet')),
-            ],
-          ),
-          // child: Builder(
-          //   builder: (context) => TextButton(
-          //     onPressed: () {
-          //       showModalBottomSheet(
-          //         context: context,
-          //         builder: (context) {
-          //           return Text('ssssds');
-          //         },
-          //       );
-          //     },
-          //     child: const Text('Open drawer'),
-          //   ),
-          // ),
-        ),
+        body: TabBarBody(),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
-          onPressed: openDrawer,
-          // child: const Icon(Icons.add_chart_outlined),
+          onPressed: openBottomSheet,
         ),
         bottomNavigationBar: BottomAppBar(
           shape: CircularNotchedRectangle(),
@@ -135,52 +47,179 @@ class _ScaffoldExampleState extends State<ScaffoldExample> {
             ),
           ),
         ),
-        // bottomSheet: BottomSheet(
-        //   builder: (context) => Container(
-        //     height: 200,
-        //     color: Colors.black38,
-        //     child: const Center(child: Text('Container in BottomSheet')),
-        //   ),
-        //   onClosing: () {},
-        // ),
       ),
     );
   }
 }
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({Key? key}) : super(key: key);
+class BottomSheet extends StatelessWidget {
+  const BottomSheet({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BottomSheet(
-      builder: (context) => Container(
-        height: 200,
-        color: Colors.black38,
-        child: const Center(child: Text('Container in BottomSheet')),
-        // decoration: const BoxDecoration(
-        //   borderRadius: BorderRadius.only(
-        //     topLeft: Radius.circular(20),
-        //     topRight: Radius.circular(20),
-        //   ),
-        // ),
-      ),
-      onClosing: () {},
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  const SecondPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Center(
-        child: Text(
-          'Second Page',
-          style: TextStyle(color: Colors.redAccent),
+    return Container(
+      height: 200,
+      child: TextButton(
+        child: const Center(
+          child: Text('Close bottom sheet'),
         ),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: CircleAvatar(
+              radius: 68,
+              backgroundColor: Colors.cyan,
+              backgroundImage: NetworkImage(
+                  'https://omoro.ru/wp-content/uploads/2018/11/rozshi_parnei-21.jpg'),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.account_box_rounded),
+            title: const Text('Profile'),
+            trailing: const Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.image_rounded),
+            title: const Text('Images'),
+            trailing: const Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.file_copy_rounded),
+            title: const Text('Files'),
+            trailing: const Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Вход'),
+                    ),
+                  ),
+                  Container(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Регистрация'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AppEndDrawer extends StatefulWidget {
+  const AppEndDrawer({Key? key}) : super(key: key);
+
+  @override
+  State<AppEndDrawer> createState() => _AppEndDrawerState();
+}
+
+class _AppEndDrawerState extends State<AppEndDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage(
+                  'https://biser-mix.ru/wp-content/uploads/2020/12/58.jpeg'),
+            ),
+            Text('hi!'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TabBarBody extends StatefulWidget {
+  const TabBarBody({Key? key}) : super(key: key);
+
+  @override
+  State<TabBarBody> createState() => _TabBarBodyState();
+}
+
+class _TabBarBodyState extends State<TabBarBody> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('App bar title!'),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.cloud_outlined),
+              ),
+              Tab(
+                icon: Icon(Icons.beach_access_sharp),
+              ),
+              Tab(
+                icon: Icon(Icons.brightness_5_sharp),
+              ),
+            ],
+          ),
+          actions: [
+            Builder(
+              builder: (context) => IconButton(
+                icon: Icon(Icons.person_outline_rounded),
+                onPressed: () {
+                  Scaffold.of(context).openEndDrawer();
+                },
+                // onPressed: toggleBottomSheet,
+              ),
+            )
+          ],
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text("It's cloudy here"),
+            ),
+            Center(
+              child: Text("It's rainy here"),
+            ),
+            Center(
+              child: Text("It's sunny here"),
+            ),
+          ],
+        ),
+        drawer: AppDrawer(),
+        endDrawer: AppEndDrawer(),
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:home_work_5/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const ScaffoldExample());
+    await tester.pumpWidget(const ScaffoldEx());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
